066a6d321dde77de31cc9a8b136279a7175cf896
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-09 16:36:08 -0500

    phase-1-control-flow-lab/
    phase-1-control-flow-lab/

--- phase-1-control-flow-lab-master/test/indexTest.js
+++ phase-1-control-flow-lab-master/test/indexTest.js
@@ -1,7 +1,13 @@
+require ( './helpers.js' );
+
 describe('index.js', function () {
   describe('scuberGreetingForFeet()', function () {
     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
       expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+    });
+
+    it('charges 20 dollars for a distance between 400 and 2000 feet', function () {
+      expect(scuberGreetingForFeet(1500)).to.equal('That will be twenty bucks.');
     });
 
     it('charges 30 dollars for a distance over 2000 feet', function () {
